a1a49ada5f6d7fc5ee8adf6e57cb96fb28456e60
Ubaid07 <ubaid.rockstar07@gmail.com>
2020-07-05 01:20:59 +0500

    Created Basic Material UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Button, Icon, AppBar, Toolbar, IconButton, Typography, makeStyles, Grid} from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  testButton: {
+    backgroundColor: "red"
+  }
+}));
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const classes = useStyles();
+return (
+        <div className={classes.root}>
+          <AppBar position="static" className={classes.testButton}>
+            <Toolbar>
+              <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" className={classes.title}>
+                News
+              </Typography>
+              <Button color="inherit" className={classes.testButton}>Login</Button>
+            </Toolbar>
+          </AppBar>
+        </div>
+      );
+    }
+   
+
 
 export default App;
